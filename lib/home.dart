875e37ab2f22bac1642a31f25498ba3c4e2960e8
bast <bastrou11@gmail.com>
2017-11-13 14:27:58 +0100

    First commit for the newest version of MyFunBox who the first version is started in kotlin and now re-adapted in flutter.

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new DrawerHeader(
+                child: new Image(
+                    image: null
+                )
+            )
+          ],
+        )
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
